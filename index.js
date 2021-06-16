f0c1c29ef22231e1de840c65621732f6ec073ec0
ecleaver <37559968+ecleaver@users.noreply.github.com>
2021-06-15 20:49:53 -0400

    Completed the game part of the test.

--- index.js
+++ index.js
@@ -1,11 +1,3 @@
-// Import stylesheets
-import './style.css';
-
-// Write Javascript code!
-const appDiv = document.getElementById('app');
-appDiv.innerHTML = `<h1>JS Starter</h1>`;
-
-
 class Game {
 
   /**
@@ -47,21 +39,49 @@ class Game {
   /**
    * Console log the winner of the battle
    */
+
+  /*
+  # Have a statement to check if the player 1s heath is greater then 0.
+  # After this display a message that player 1 is winner and their name
+  # esle have a statement to check if the player 2s heath is greather then 0.
+  # After this display a message that player 2 is winner and their name
+  */
   declareWinner(){
+    if(this.player1.health > 0) {
+      console.log(`Player 1: ${this.player1.name} is the winner!!`)
+    } else if(this.player2.health > 0) {
+      console.log(`Player 2: ${this.player2.name} is the winner!!`)
+    }
+
   }
 
   /**
    * If player 1 or player 2 health is below 0
    * Mark theEnd true, to stop the game
    */
+
+   /*
+   # Check if the player 1 health is below 0 or if player 2s health is below 0
+   # if either returns true change the value of the end to true.  
+   */
   checkTheEnd(){
+    if(this.player1.health < 0 || this.player2.health < 0) {
+      this.theEnd = true;
+    }
   }
 
   /**
    * Console log the name and health of both players
    * Ex: Player 1: 100 | Player 2: 50
    */
+
+  /*
+  # displays a message with the players health 
+  */
   playerStatus(){ 
+    console.log("\n\nCurrent Health")
+    console.log(`Player 1: ${this.player1.health}`)
+    console.log(`Player 2: ${this.player2.health}`)
 
   }
 
@@ -69,38 +89,89 @@ class Game {
    * Reset health of player 1 and player 2 to 100
    * Reset theEnd to false
    */
+
+  
   reset(){ 
+    this.player1.health = 100;
+    this.player2.health = 100;
+    this.theEnd = false;
   }
 
   /**
    * Generate a random number between 1 and 10 using Math.random()
    * Use that number to decrease health from player 2
    */
+
+  /*
+  # first you have to get a random number between 1 and 10 
+  # After this is is taken away from player 2s health
+  */
   pl1AttackPl2(){ 
+
+    let damage = Math.floor(Math.random() * 10) + 1;
+    this.player2.health-= damage;
+
   }
 
   /**
    * Generate a random number between 1 and 10 using Math.random()
    * Use that number to decrease health from player 1
    */
+
+    /*
+  # first you have to get a random number between 1 and 10 
+  # After this is is taken away from player 1s health
+  */
   pl2AttackPl1(){ 
+    let damage = Math.floor(Math.random() * 10) + 1;
+    this.player1.health-= damage;
   }
 
   /**
    * Generate a random number between 1 and 5 using Math.random()
    * Use that number to increase health of player 1
    */
+  /*
+  # before healing the player you must check health if it is 100;
+  # Only if the health is less then 100 will the player be healed.  
+  # first you generate a number between 1 and 5
+  # Then you increate the health of player 1 by 5
+  */
+
   pl1Heal(){
+    if(this.player1.health < 100) {
+      let heal = Math.floor(Math.random() * 5) + 1;
+      this.player1.health+= heal;
+      if(this.player1.health > 100) {
+        this.player1.health = 100;
+      }
+    } 
   }
 
   /**
    * Generate a random number between 1 and 5 using Math.random()
    * Use that number to increase health of player 2
    */
+  /*
+  # before healing the player you must check health if it is 100;
+  # Only if the health is less then 100 will the player be healed.  
+  # first you generate a number between 1 and 5
+  # Then you increate the health of player 1 by 5
+  */
+
   pl2Heal(){ 
+    if(this.player2.health < 100) {
+      let heal = Math.floor(Math.random() * 5) + 1;
+      this.player2.health+= heal;
+      if(this.player2.health > 100) {
+        this.player2.health = 100;
+      }
+    } 
   }
 
 }
 
 // Initialize the class here
 // Call the start function of the game
+let game = new Game("John", "Jake");
+game.start();
